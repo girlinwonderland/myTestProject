898a408768948afa5446f4ffdef41c711a01a2dd
eakossova <eakossova@domclick.ru>
2024-10-13 21:03:53 +0300

    ts config

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # myTestProject
 
 TODO
-1. webpack
-2. sass/css
-4. tsconfig
 5. eslinter
 6. stylelinter
 7. env
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import styles from './App.module.scss';
+import styles from './App.scss';
 
 // 1. if we use scss
 // import './styles.scss'; => <div className="title" />
 
 // 2. if we use styles.modules.scss
-// import styles from './styles.module.scss'; => <div className="title" />
+// import styles from './styles.module.scss'; => <div className={styles.title} />
 
-console.log(styles)
+// 3. Но если указать options для css-loader то второй варинат будет работать из без модуля
 
 const App = () => {
   return <h1 className={styles.title}>Hello</h1>
